8f24fbc4536a3f9ed2f5393da6bef2e1aa0a717e
Adam Oken <Adam_Oken@Adams-MacBook-Air.local>
2015-11-17 15:33:55 -0700

    Update 2015-11-17T22:33:49.823Z

--- script.js
+++ script.js
@@ -10,21 +10,28 @@ $(function(){
 			dataType: 'json',
 			url: 'http://api.wordnik.com:80/v4/word.json/' + query + '/relatedWords?useCanonical=false&relationshipTypes=rhyme&limitPerRelationshipType=' + number + '&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5',
 			success: function(data){
-				// console.log(data)
+				console.log(data)
 				//console.log(data[0].words)
 				var rhymeWords = [];
-		
-				for(i=0; i<data[0].words.length; i++){
-					//console.log(data[0].words[i])
+				if(data[0]){
+					for(i=0; i<data[0].words.length; i++){
 					rhymeWords.push(data[0].words[i])
+					//console.log(data[0].words[i])
 				}
+					}
+					else{
+						alert("Do not enter compound words or words with symbols/punctuation")
+					}
 				$('#rhymeBox').empty()
 				for(i=0; i<rhymeWords.length; i++){
 					$('#rhymeBox').append('<p>' + rhymeWords[i] + '<p>')
 				};
 				console.log(rhymeWords)
-			}
-		})
+			 },
+			 error: function (ajaxContext) {
+        alert("Enter a word")
+    }
+			})
 		return false
 		}
 		$('#subBut').on('click', wordInput)
