71e48331c9416effbf1b7e19ff2508553b6e784a
Adam Oken <Adam_Oken@Adams-MacBook-Air.local>
2015-11-17 11:39:48 -0700

    fix

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ $(function(){
 				// console.log(data)
 				//console.log(data[0].words)
 				var rhymeWords = [];
-				if(undefined){
+		
 				for(i=0; i<data[0].words.length; i++){
 					//console.log(data[0].words[i])
 					rhymeWords.push(data[0].words[i])
@@ -22,15 +22,9 @@ $(function(){
 				for(i=0; i<rhymeWords.length; i++){
 					$('#rhymeBox').append('<p>' + rhymeWords[i] + '<p>')
 				};
-				}
-					alert("Do not enter compound words")
-				}
 				console.log(rhymeWords)
-			 },
-			 error: function (ajaxContext) {
-        alert("Enter a word")
-    }
-			})
+			}
+		})
 		return false
 		}
 		$('#subBut').on('click', wordInput)
